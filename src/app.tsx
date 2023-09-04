1acc3152d33620925065929344431d551e85fb04
Aleix Casanovas <acasanov@redhat.com>
2023-09-04 12:02:08 +0200

    Config file

--- src/app.tsx
+++ src/app.tsx
@@ -7,12 +7,12 @@ import Split from 'react-split';
 import ProgressHeader from './progress-header';
 import './app.css'
 
-type service = {name: string, url?: string, port?: string, secondary_port?: string, path?: string, secondary_path?: string, secondary_url?: string};
+type tab = {name: string, url?: string, port?: string, secondary_port?: string, path?: string, secondary_path?: string, secondary_url?: string};
 
 const protocol = window.location.protocol;
 const hostname = window.location.hostname;
 
-const createUrlsFromVars = (vars: service): service  => {
+const createUrlsFromVars = (vars: tab): tab  => {
     if (vars.url) {
         return vars;
     }
@@ -32,15 +32,15 @@ export default function() {
     const ref = useRef();
     const instructionsPanelRef = useRef();
     const [session, setSession] = useState<Session>(null);
-    const {data, error} = useSWR('./lab-config.yml', (url) => fetch(url).then(r => r.text()), { suspense: true });
-    const config = yaml.load(data) as {showroom_version: string, showroom_modules: {name: string, validation_script?: string}[], showroom_services: service[], showroom_name: string, antora_dir?: string};
-    const modules = config.showroom_modules;
-    const antoraDir = config.antora_dir || 'antora';
-    const version = config.showroom_version;
-    const s_name = config.showroom_name;
-    const services = config.showroom_services.map(s => createUrlsFromVars(s));
+    const {data, error} = useSWR('./nookbag.yml', (url) => fetch(url).then(r => r.text()), { suspense: true });
+    const config = yaml.load(data) as {antora: { modules: {name: string, validation_script?: string}[], name: string, dir?: string, version: string }, tabs: service[]};
+    const modules = config.antora.modules;
+    const antoraDir = config.antora.dir || 'antora';
+    const version = config.antora.version;
+    const s_name = config.antora.name;
+    const tabs = config.tabs.map(s => createUrlsFromVars(s));
     const [progress, setProgress] = useState({inProgress: [], completed: [], notStarted: modules.map(x => x.name), current: modules[0].name});
-    const [currentService, setCurrentService] = useState(services?.[0]);
+    const [currentTab, setCurrentTab] = useState(tabs?.[0]);
     const [iframeModule, setIframeModule] = useState(modules[0].name);
     const currIndex = modules.findIndex(m => m.name === progress.current);
     const initialFile = `./${antoraDir}/${s_name}/${version}/${iframeModule}.html`
@@ -88,8 +88,8 @@ export default function() {
         }
     }
 
-    function handleTabClick(service: service) {
-        setCurrentService(service)
+    function handleTabClick(tab: tab) {
+        setCurrentTab(tab)
     }
 
     function handleNext() {
@@ -129,20 +129,20 @@ export default function() {
                     </div>
                     <div className="split right">
                         <div className="tab">
-                            {services.map(s => <Button variant="plain" isActive={s.name === currentService.name} key={s.name} className="tablinks" onClick={() => handleTabClick(s)}>{s.name}</Button>)}
+                            {tabs.map(s => <Button variant="plain" isActive={s.name === currentTab.name} key={s.name} className="tablinks" onClick={() => handleTabClick(s)}>{s.name}</Button>)}
                         </div>
                         <div className="tabcontent">
-                            {currentService.secondary_url ?
+                            {currentTab.secondary_url ?
                             <>
                                 <div className="split top">
-                                    <iframe src={currentService.url} width="100%"></iframe>
+                                    <iframe src={currentTab.url} width="100%"></iframe>
                                 </div>
                                 <div className="split bottom">
-                                    <iframe src={currentService.secondary_url} width="100%"></iframe>
+                                    <iframe src={currentTab.secondary_url} width="100%"></iframe>
                                 </div>
                             </>
                             :
-                            <iframe src={currentService.url} height="100%" width="100%"></iframe>}
+                            <iframe src={currentTab.url} height="100%" width="100%"></iframe>}
                         </div>
                     </div>
                 </Split>
