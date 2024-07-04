b6cdc556c0ce719732d2cbef0cceae57cb203ea9
Aleix Casanovas <acasanov@redhat.com>
2024-07-04 15:27:39 +0200

    fixup! fixup! fixup! feat: Show solve button

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import '@patternfly/react-core/dist/styles/base.css';
 
-import React, {Suspense} from "react";
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 import { ErrorBoundary } from 'react-error-boundary';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<Suspense fallback={<div>loading...</div>}>
-  <ErrorBoundary
-    fallbackRender={() => <div>Configuration file not defined</div>}
-  >
-    <App />
-  </ErrorBoundary>
-</Suspense>);
+root.render(
+  <Suspense fallback={<div>loading...</div>}>
+    <ErrorBoundary fallbackRender={() => <div>Configuration file not defined</div>}>
+      <App />
+    </ErrorBoundary>
+  </Suspense>
+);
--- src/progress-bar.tsx
+++ src/progress-bar.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React from 'react';
 import './progress-bar.css';
 
-export default function({modules, progress}: {modules: {name: string, validation_script?: string}[], progress: {current: string, inProgress: string[], notStarted: string[], completed: string[]}}) {
-    return <div className="progress-bar__wrapper">
-        <span className="progress-bar__label">Progress</span>
-        <div className="progress-bar">
-            {modules.map((m) => <div className={m.name === progress.current ? 'progress-bar__item is-current': progress.completed.includes(m.name) ? 'progress-bar__item completed' : 'progress-bar__item not-started'}></div>)}
-        </div>
+export default function ({
+  modules,
+  progress,
+}: {
+  modules: { name: string; validation_script?: string }[];
+  progress: { current: string; inProgress: string[]; notStarted: string[]; completed: string[] };
+}) {
+  return (
+    <div className="progress-bar__wrapper">
+      <span className="progress-bar__label">Progress</span>
+      <div className="progress-bar">
+        {modules.map((m) => (
+          <div
+            className={
+              m.name === progress.current
+                ? 'progress-bar__item is-current'
+                : progress.completed.includes(m.name)
+                ? 'progress-bar__item completed'
+                : 'progress-bar__item not-started'
+            }
+          ></div>
+        ))}
+      </div>
     </div>
+  );
 }
